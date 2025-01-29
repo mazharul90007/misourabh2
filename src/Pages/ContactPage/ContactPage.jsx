f77473fe54425afd1bac3095a0eb305841ba591e
mazharul90007 <mazharul90007@gmail.com>
2025-01-29 20:17:34 +0600

    Contact Page added

---
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,13 @@
+import Contact from "../Home/Contact/Contact";
+
+
+const ContactPage = () => {
+    return (
+        <div className="min-h-[calc(100vh-192px)]">
+            <Contact></Contact>
+            
+        </div>
+    );
+};
+
+export default ContactPage;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import Error from "../Pages/Error/Error";
+import ContactPage from "../Pages/ContactPage/ContactPage";
 
 const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home></Home>
+        },
+        {
+          path: '/contact',
+          element: <ContactPage></ContactPage>
         }
       ]
     },
